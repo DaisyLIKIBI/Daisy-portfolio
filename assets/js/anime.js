b70c5038b9be068510799a1b85ac873becff8ba4
DaisyLIKIBI <106149267+DaisyLIKIBI@users.noreply.github.com>
2022-10-19 11:58:26 +0100

    'portfolio'

---
+++ assets/js/anime.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.anime', {
+    strings:[
+        '',
+        'Daisy LIKIBI',
+        'Referente Digitale',
+        'Community Manager',
+        'Spécialiste dans le Web Design',
+        'Spécialiste en Gestion de Projet',
+      ],
+      typeSpeed: 100,
+      backSpeed: 200,
+      loop: true
+   })
